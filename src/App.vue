0362685cdfed36e38d42eccf80a9314335018179
A Anand <a.anand.91119@gmail.com>
2024-07-14 13:49:17 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 container mx-auto">
+    <header class="p-8">
+      <nav>
+
+      </nav>
+    </header>
+  </div>
+</template>
